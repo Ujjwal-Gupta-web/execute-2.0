fe804c154a1e9f3cc9afd7da306959e4816cdcd8
J-a-yy <mvk1407@gmail.com>
2022-05-28 16:40:30 +0530

    added tailwind

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,22 @@
 import React from 'react';
-
 import Navbar from '../components/Navbar'
+import img1 from '../assets/img1.jpg'
+import '../assets/index.css'
 
 export default function Home(){
   return(
 
     <div className="home_page">
       <div className="title">
-        <h1 >Event Wala</h1>
+        <h1 className="font-luckyOne text-[4rem] p-5 m-4">Event Wala</h1>
       </div>
 
       <div className="nav">
         <Navbar />
+      </div>
+      
+      <div className="hero_section float-right m-20 shadow-xl rounded-xl">
+        <img src={img1} className="img1" />
       </div>
     </div>
   )
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          luckyOne :  ['Calistoga', 'cursive']
+        }
+      },
+    },
+    plugins: [],
+  }
+  
