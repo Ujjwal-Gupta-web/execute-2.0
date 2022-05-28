58423a5ad2907bb9895af359d47d12b10700ad98
J-a-yy <mvk1407@gmail.com>
2022-05-29 00:24:13 +0530

    feat: added business and user controller

--- frontend/src/controllers/user.js
+++ frontend/src/controllers/user.js
@@ -4,6 +4,38 @@ export const auth_user = async(obj) => {
     const res = await fetch(`${base}/api/user/auth`,{
         method: "POST",
         body: JSON.stringify(obj),
-        headers
-    })
-}
+        headers : {
+            "Content-Type": "application/json",
+        },
+    });
+    const ans = await res.json();
+    return ans;
+};
+
+
+export const login_user = async(obj) => {
+    const res = await fetch(`${base}/api/user/login`,{
+        method : 'POST',
+        body : JSON.stringify(obj),
+        headers : {
+            "Content-Type": "application/json"
+        },
+    });
+    const ans = await res.json();
+    return ans;
+};
+
+
+export const signup_user = async(obj) =>{
+    const res = await fetch(`${base}/api/user/signup`,{
+        method : 'POST',
+        body : JSON.stringify(obj),
+        headers : {
+            "Content-Type": "application/json"
+        }
+    });
+    const ans = await res.json();
+    return ans;
+};
+
+
